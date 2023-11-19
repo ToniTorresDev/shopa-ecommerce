b36e236f3edb99543469b0837a531996a85897a1
Toni Torres Cuenca <ttorres@aduxia.com>
2023-11-19 17:44:47 +0100

    Change product icon when it's in the cart and limit addition to one

--- src/app/components/ProductCard/index.jsx
+++ src/app/components/ProductCard/index.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react"
 import { ShoppingCartContext } from "../../context"
+import { CheckIcon } from "@heroicons/react/24/solid"
 
 function ProductCard(data) {
   const context = useContext(ShoppingCartContext)
@@ -15,26 +16,41 @@ function ProductCard(data) {
     context.setShoppingCart([...context.shoppingCart, product])
   }
 
-  return (
-    <div
-      onClick={() => showProduct(data.data)}
-      className="bg-white cursor-pointer w-56 h-60 rounded-lg"
-    >
-      <figure className="relative mb-2 w-full h-4/5">
-        <span className="absolute bottom-0 left-0 px-3 py-0.5 bg-white/60 rounded-lg text-black text-xs m-2">
-          {data.data.category.name}
-        </span>
-        <img
-          className="w-full h-full object-cover rounded-lg"
-          src={data.data.image}
-          alt="headphones"
-        />
+  const renderIcon = (id) => {
+    const isInCart = !!context.shoppingCart.find((product) => product.id === id)
+
+    if (!isInCart) {
+      return (
         <button
-          className="absolute top-0 right-0 m-3 p-1 flex justify-center items-center bg-white w-6 h-6 rounded-full"
+          className="absolute right-0 top-0 m-3 flex h-6 w-6 items-center justify-center rounded-full bg-orange-300 p-1"
           onClick={() => addProductToCart(event, data.data)}
         >
           +
         </button>
+      )
+    }
+    return (
+      <button className="absolute right-0 top-0 m-3 flex h-6 w-6 items-center justify-center rounded-full bg-green-600 p-1">
+        <CheckIcon className="h-6 w-6 text-white" />
+      </button>
+    )
+  }
+
+  return (
+    <div
+      onClick={() => showProduct(data.data)}
+      className="h-60 w-56 cursor-pointer rounded-lg bg-white"
+    >
+      <figure className="relative mb-2 h-4/5 w-full">
+        <span className="absolute bottom-0 left-0 m-2 rounded-lg bg-white/60 px-3 py-0.5 text-xs text-black">
+          {data.data.category.name}
+        </span>
+        <img
+          className="h-full w-full rounded-lg object-cover"
+          src={data.data.image}
+          alt="headphones"
+        />
+        {renderIcon(data.data.id)}
       </figure>
       <p className="flex justify-between">
         <span className="text-sm font-light">{data.data.title}</span>
