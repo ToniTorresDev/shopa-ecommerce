f28ddc1c00203049a1befc67f6eb63c9e5ea9ee6
Toni Torres Cuenca <ttorres@aduxia.com>
2023-12-08 11:12:47 +0100

    fix problems with decimals in total price

--- src/app/utils/index.js
+++ src/app/utils/index.js
@@ -1,3 +1,3 @@
 export const getTotalPrice = (products) => {
-  return products.reduce((sum, product) => sum + product.price, 0)
+  return products.reduce((sum, product) => sum + product.price, 0).toFixed(2)
 }
